54ed56c64ec514294d4411c7568aa594240cd509
Simon Borgström <borgstrom.simon@gmail.com>
2016-12-01 11:59:15 +0100

    added files

---
+++ index.android.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+import { styles } from './styles/index';
+import Camera from 'react-native-camera';
+
+
+export default class CameraMapper extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+
+      </View>
+    );
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+
+}
+
+
+AppRegistry.registerComponent('CameraMapper', () => CameraMapper);
